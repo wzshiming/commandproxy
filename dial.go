1d3ee813f960b38263f8f25f7ab0f9e3c716cd5f
wzshiming <wzshiming@foxmail.com>
2020-05-25 21:52:38 +0800

    add format for dial proxy command

--- dial.go
+++ dial.go
@@ -7,7 +7,7 @@ import (
 
 type DialProxyCommand []string
 
-func (p *DialProxyCommand) DialContext(ctx context.Context, network string, address string) (net.Conn, error) {
+func (p *DialProxyCommand) Format(network string, address string) ([]string, error) {
 	host, port, err := net.SplitHostPort(address)
 	if err != nil {
 		return nil, err
@@ -21,6 +21,14 @@ func (p *DialProxyCommand) DialContext(ctx context.Context, network string, addr
 	copy(proxy, *p)
 	for i := range proxy {
 		proxy[i] = ReplaceEscape(proxy[i], m)
+	}
+	return proxy, nil
+}
+
+func (p *DialProxyCommand) DialContext(ctx context.Context, network string, address string) (net.Conn, error) {
+	proxy, err := p.Format(network, address)
+	if err != nil {
+		return nil, err
 	}
 	cp := ProxyCommand(ctx, proxy[0], proxy[1:]...)
 	return cp.Stdio()
